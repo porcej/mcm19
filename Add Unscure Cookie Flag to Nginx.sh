293e3c562dedcec7469ed28f2b7bb4c6419213f9
Joe Porcelli <porcej@users.noreply.github.com>
2021-08-14 16:33:39 -0400

    Add files via upload

---
+++ Add Unscure Cookie Flag to Nginx.sh
@@ -0,0 +1,13 @@
+# Install Dependencies
+sudo apt-get install git libgd-dev libgeoip-dev libxslt-dev libssl-dev libpcre3 libpcre3-dev
+
+mkdir ~/build
+cd ~/build
+
+nginx -v
+
+wget http://nginx.org/download/nginx-1.14.2.tar.gz
+
+sudo mkdir /etc/nginx/modules
+sudo cp ~/build/nginx-1.14.2/objs/ngx_http_unsecure_cookie_filter_module.so /usr/share/nginx/modules/
+
